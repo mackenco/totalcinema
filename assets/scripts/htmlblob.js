84208a56e73a20966dae96e6f3a10072cd63fbb0
Colin <colinwmackenzie@gmail.com>
2014-03-23 00:44:58 -0400

    moving files around

---
+++ assets/scripts/htmlblob.js
@@ -0,0 +1,161 @@
+var getHtmlBlob = function() {
+  return (function () {/*
+       <div class="tabs">
+          
+         <div class="tab">
+             <input type="radio" id="tab-1" name="tab-group-1" checked>
+             <label for="tab-1">About</label>
+             
+             <div class="content">
+                  <h1 class="content-h">Total Cinema 360</h1>
+
+                  <p class="main-content-p">Total Cinema 360 is a full service production company specializing in 360 degree video and virtual reality technology. We create unprecedented, interactive experiences that allow you to look beyond the edges of the frame and relieve the action as if you were there.</p>
+
+                 <h2 class="content-h">What is 360 Video?</h2>
+
+                  <p class="content-p">360 Video is a new media technology that uses multiple camera lenses to capture different sides of a space simultaneously and stitch the images together to form a seamless environment. At playback, users control where they are looking from a fixed point in a visual sphere, resulting in an unparalleled level of immersion.</p>
+
+                  <h2 class="content-h">How can I watch 360 Videos? </h2>
+
+                  <p class="content-p">Our content is available on computers, mobile devices, tablets, and the OCULUS RIFT, a next-generation virtual reality headset. The interface is different depending on the device. To adjust your view on the computer, click and drag with the mouse or use the keypad. For mobile phones and tablets, use the touchscreen or physically move the device in the direction you wish to look. The Oculus Rift responds to the movements of your head.</p>
+
+                  <h2 class="content-h">What is the Oculus Rift? </h2>
+
+                  <p class="content-p">The Oculus Rift is a Head Mounted Display (virtual reailty goggles) developed by Oculus VR for high quality gaming and virtual reality experiences.  We create all our 360 videos specifically with the Oculus Rift in mind as the optimal viewing medium. Oculus Rift Development Kits are currently available to order directly from Oculus VR. A release date for the commercial version of the Rift has not been announced.</p>
+
+                  <h2 class="content-h">Is there 360 Sound? </h2>
+
+                  <p class="content-p">Yes! We can design audio environments that are responsive to your perspective in the video. So, if two conversations are happening simultaneously, we can make it so you only hear the people you're paying attention to. Look this way, hear one thing. Look that way, hear another. Applying this technique is optional. The further development of 360 audio recording and design technologies is one of our top priorities. You can download our 360 Sound Demo along with the Total Cinema 360 Oculus Player to hear it for it yourself.</p>
+
+                  <h2 class="content-h">Can you watch 360 Videos without the 360 element? </h2>
+
+                  <p class="content-p">Yes. Any 360 Video can be turned into traditional video formats. You can even choose the path the camera follows and make different versions of the same video.</p>
+
+                  <h2 class="content-h">What type of content do you create with 360 video? </h2>
+
+                  <p class="content-p">We are currently in development on several fiction and non-fiction video projects across all platforms. 360 Video is perfect for storytelling, live events, advertising, guided tours, training services, mapping services, conferencing, and even video games. We believe that anything you can capture with traditional video can benefit from 360 technology. </p>
+
+                  <h2 class="content-h">What are you doing next? </h2>
+
+                  <p class="content-p">Our main goal right now is to come up with as many applications for 360 video as possible. We will soon be launching a remodeled version of our player with additional features and interface options, as well as several new videos to go along with it. We want our content to be available on a wide range of devices and in a wide variety of ways. Innovative technologies like the Rift are definitely a priority. We also believe 360 cameras can get much, much better. </p>
+
+                  <h2 class="content-h">Can you live stream 360 videos? </h2>
+
+                  <p class="content-p">Yes.</p>
+
+                  <h2 class="content-h">Where are you located? </h2>
+
+                  <p class="content-p">Total Cinema 360 is based in New York City. 
+
+                  <h2 class="content-h">Do you sell cameras? </h2>
+
+                  <p class="content-p">No, we do not sell hardware at this time. New 360 cameras are constantly in development and we will make them available as soon as we have settled on a long term model.</p>
+
+                  <h2 class="content-h">How do I make my own 360 videos? </h2>
+
+                  <p class="content-p">We are a full service production team and it is our pleasure to help make your 360 dreams a reality. To hire us, click here.</p>
+             </div> 
+         </div>
+          
+         <div class="tab">
+             <input type="radio" id="tab-2" name="tab-group-1">
+             <label for="tab-2">Services</label>
+             
+             <div class="content">
+                  <h1 class="content-h">Production Services</h1>
+
+                  <p class="main-content-p">360 Video is perfect for storytelling, live events, advertising, guided tours, training services, mapping services, conferencing, and even video games. We believe that anything you can capture with traditional video can benefit from 360 technology. </p>
+
+                  <p class="main-content-p">We can help you make your project a reality. We provide end-to-end production packages (capture to deliver) and individual equipment and operation services.</p>
+
+                  <h2 class="content-h">End-to-End 360° Production</h2>
+
+                  <p class="content-p">Our trained 360° Cinematographers accompany all equipment to ensure expert operation on set. 360° Editors 'stich' your footage into fully immersive images and create the finished product. We provide the software necessary to play and deliver your content on any device - desktop, laptop, tablet, or smartphone.</p>
+
+                  <h2 class="content-h">Virtual Reality Optimization</h2>
+
+                  <p class="content-p">Customize your 360 content for virtual reality headsets and other cutting-edge immersive technologies with our Total Cinema 360 Oculus Player.</p>
+
+                  <h2 class="content-h">360° Event Recording and Live Streaming</h2>
+                  
+                  <p class="content-p">With our expert capturing and processing services, we also provide the ability to stream events live over the Internet directly on your website or app platform. </p>
+             </div> 
+         </div>
+          
+          <div class="tab">
+             <input type="radio" id="tab-3" name="tab-group-1">
+             <label for="tab-3">Contact Us</label>
+           
+             <div class="content">
+
+              <h1 class="content-h">Contact Us</h1>
+
+              <p class="main-content-p">To hire us, or if you have any questions about our 360 technology and services, contact us at <a href="mailto:contact@totalcinema360.com" target="_blank">contact@totalcinema360.com</a></p>
+
+              <h2 class="content-h">Our Mission</h2>
+
+              <p class="content-p">Total Cinema 360 believes in the untapped potential of 360 video and virtual reality technology to redefine the creative boundaries of audio/visual storytelling by allowing personalized engagement with fully immersive worlds and interactive experiences never before possible with conventional video formats.</p>
+
+              <h2 class="content-h">Team</h2>
+
+              <div id="team-wrapper" class="bgrid-halves cf">
+
+                <div class="member">
+                    <img src="assets/images/adrian.jpg" width="100" height="151" alt="adrian">
+                  
+                  <div class"member-name">
+                     <h5><a href="mailto:adrian@totalcinema360.com" target="_blank">Adrian Vasquez de Velasco</a></h5>
+                     <span>Co-Founder</span>
+                  </div>
+
+                  <p>Adrian is a writer, director, and producer currently in development on a feature film designed for 360 degree video. His films have screened at the Smithsonian Institute, the World Sustainability Forum, and the Environmental Film Festival in Washington, DC</p>
+
+                </div>
+
+                <div class="member">
+                    <img src="assets/images/craig.jpg" width="100" height="151" alt="adrian">
+                  
+                  <div class"member-name">
+                     <h5><a href="mailto:craig@totalcinema360.com" target="_blank">Craig Gilbert</a></h5>
+                     <span>Co-Founder</span>
+                  </div>
+
+                  <p>Craig is a writer, director, and producer currently in development on a feature film designed for 360 degree video. His films have been featured at international film festivals including Cannes, Warsaw, and Palm Beach.</p>
+
+                </div>
+
+                <div class="member">
+                    <img src="assets/images/alex.jpg" width="100" height="151" alt="adrian">
+                  
+                  <div class"member-name">
+                     <h5><a href="mailto:alex@totalcinema360.com" target="_blank">Alex Grau</a></h5>
+                     <span>Technology Strategist</span>
+                  </div>
+
+                  <p>Alex is a software and hardware engineer with 6 years of experience working with NYC tech start-ups. He studied computer science and psychology at NYU, where his published research focused on human-computer interaction. In addition to software consulting for Barnes & Nobel, Verizon, and Actor Machine, he worked as an engineer at TouchCo from founding to successful acquisition and is a founding member of Tactonic Technologies.</p>
+
+                </div>
+
+                <div class="member">
+                  <img src="assets/images/nick.jpg" width="100" height="151" alt="adrian">
+                
+                  <div class"member-name">
+                     <h5><a href="mailto:nick@totalcinema360.com" target="_blank">Nicholas DiMichele</a></h5>
+                     <span>360 Sound Director</span>
+                  </div>
+
+                  <p>Nick is a music producer, sound engineer, voice actor and musician living in New York. He records live and in studio for music events, albums, films and TV. He has appeared on 30 Rock, is a character on Pokemon!, and tours as a member of alternative band Ghost & Goblin.</p>
+
+                </div>
+
+              </div>
+
+             </div> 
+
+
+         </div>          
+
+      </div>   
+
+      */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+};
